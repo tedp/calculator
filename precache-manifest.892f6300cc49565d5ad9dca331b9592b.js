cc9faeeb3671ec5c5a613ee66814043a2a57b46d
Ted Pritchard <github@pritchard.uk.net>
2020-04-15 07:33:53 +0100

    Updates

---
+++ precache-manifest.892f6300cc49565d5ad9dca331b9592b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af2610fbc78f2675a4f7c1af21931c7",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "422a0bb98e294f16fe60",
+    "url": "/calculator/static/css/main.a5c26fa0.chunk.css"
+  },
+  {
+    "revision": "d5c26d3d6bd503d00358",
+    "url": "/calculator/static/js/2.acb358a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.acb358a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422a0bb98e294f16fe60",
+    "url": "/calculator/static/js/main.0382a0a3.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
